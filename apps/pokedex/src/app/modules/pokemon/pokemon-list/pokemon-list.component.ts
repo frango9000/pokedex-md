daf5797e4e10fa88152f8bcaf46ab092ae83f745
Francisco Sanchez <frango9000@gmail.com>
2023-05-11 18:05:50 +0200

    chore(project): add sorting to pokemon list

--- apps/pokedex/src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
+++ apps/pokedex/src/app/modules/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,22 +1,24 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { PxPokemon } from '@pokedex-md/domain';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { take } from 'rxjs/operators';
 import { PokemonService } from '../../../shared/services/pokemon.service';
-import { PokemonListDatasource } from './pokemon-list.datasource';
+import { GenericDatasource } from '../../../shared/utils/generic-datasource';
 
 @UntilDestroy()
 @Component({
   selector: 'pokedex-pokemon-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, NgOptimizedImage, InfiniteScrollModule],
+  imports: [CommonModule, MatTableModule, NgOptimizedImage, InfiniteScrollModule, MatSortModule],
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent {
-  protected readonly dataSource: PokemonListDatasource = new PokemonListDatasource();
+  protected readonly dataSource: GenericDatasource<PxPokemon> = new GenericDatasource<PxPokemon>();
 
   protected readonly displayedColumns: string[] = ['id', 'sprite', 'name', 'generation', 'types'];
 
@@ -24,6 +26,6 @@ export class PokemonListComponent {
     this.pokemonService
       .getAll()
       .pipe(take(1))
-      .subscribe((data) => (this.dataSource.resources$ = data));
+      .subscribe((data) => (this.dataSource.resources = data));
   }
 }
--- apps/pokedex/src/app/shared/utils/generic-datasource.ts
+++ apps/pokedex/src/app/shared/utils/generic-datasource.ts
@@ -4,22 +4,21 @@ import { untilDestroyed } from '@ngneat/until-destroy';
 import { BehaviorSubject, combineLatestWith, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export abstract class AbstractDatasource<T> implements DataSource<T> {
+export class GenericDatasource<T> implements DataSource<T> {
   private readonly _resources$: BehaviorSubject<T[]> = new BehaviorSubject<T[]>([]);
-  private readonly _sort$: BehaviorSubject<Sort | null> = new BehaviorSubject<Sort | null>({
-    active: 'id',
-    direction: 'desc',
-  });
   private readonly _filters$: BehaviorSubject<Filters> = new BehaviorSubject<Filters>({});
-
   private readonly _page$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   private readonly _pageSize: number = 40;
+  private readonly _sort$: BehaviorSubject<Sort | null> = new BehaviorSubject<Sort | null>({
+    active: 'id',
+    direction: 'asc',
+  });
 
   private readonly _datasource$: Observable<T[]> = this._resources$.pipe(
     combineLatestWith(this._filters$),
-    map(([resources, filters]) => resources.filter(this.resourceFilterer(filters))),
+    map(([resources, filters]) => resources.filter(this._filter(filters))),
     combineLatestWith(this._sort$),
-    map(([resources, sort]) => resources.sort(this.resourceSorter(sort))),
+    map(([resources, sort]) => resources.sort(this._sort(sort))),
     combineLatestWith(this._page$),
     map(([resources, page]) => resources.slice(0, this._pageSize * (page + 1))),
     untilDestroyed(this, 'disconnect'),
@@ -33,6 +32,7 @@ export abstract class AbstractDatasource<T> implements DataSource<T> {
     this._resources$.complete();
     this._sort$.complete();
     this._filters$.complete();
+    this._page$.complete();
   }
 
   get datasource$(): Observable<T[]> {
@@ -43,25 +43,67 @@ export abstract class AbstractDatasource<T> implements DataSource<T> {
     return this._resources$.pipe(map((page) => page.length));
   }
 
-  set resources$(resources: T[]) {
+  get resources$(): Observable<T[]> {
+    return this._resources$.asObservable();
+  }
+
+  set resources(resources: T[]) {
     this._resources$.next(resources);
+  }
+
+  get sort$(): Observable<Sort | null> {
+    return this._sort$.asObservable();
   }
 
   set sort(sort: Sort) {
     this._sort$.next(sort);
   }
 
+  get filters$(): Observable<Filters> {
+    return this._filters$.asObservable();
+  }
+
   set filters(filters: Filters) {
     this._filters$.next(filters);
   }
 
-  public nextPage(): void {
+  get page$(): Observable<number> {
+    return this._page$.asObservable();
+  }
+
+  nextPage(): void {
     this._page$.next(this._page$.value + 1);
   }
 
-  protected abstract resourceFilterer(filters: Filters): (resource: T) => boolean;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected _filter(filters: Filters): (resource: T) => boolean {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return function (e: T) {
+      return true;
+    };
+  }
 
-  protected abstract resourceSorter(sort: Sort | null): (a: T, b: T) => number;
+  protected _sort(sort: Sort | null): (a: T, b: T) => number {
+    return function (a: T, b: T) {
+      let comparison = 0;
+      if (sort) {
+        const propA = a[sort.active as keyof T];
+        const propB = b[sort.active as keyof T];
+
+        if (propA > propB) {
+          comparison = 1;
+        } else if (propA < propB) {
+          comparison = -1;
+        }
+
+        if (sort.direction === 'desc') {
+          comparison = comparison * -1;
+        }
+      }
+
+      return comparison;
+    };
+  }
 }
 
 export interface Filters {
