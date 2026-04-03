c3c3a65275d53a6a655e823a66fa5980a4b332bf
Francisco Sanchez <frango9000@gmail.com>
2026-04-03 21:35:32 +0200

    feat: add tooltips to language and version group pickers for improved user accessibility and context

--- apps/pokedex/src/app/core/components/language-picker.component.ts
+++ apps/pokedex/src/app/core/components/language-picker.component.ts
@@ -3,13 +3,14 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
 import { TranslocoModule } from '@jsverse/transloco';
 import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'pokedex-language-picker',
   standalone: true,
-  imports: [CommonModule, MatMenuModule, MatIconModule, MatButtonModule, TranslocoModule, NgOptimizedImage],
+  imports: [CommonModule, MatMenuModule, MatIconModule, MatButtonModule, TranslocoModule, NgOptimizedImage, MatTooltip],
   templateUrl: './language-picker.component.html',
   styleUrls: ['./language-picker.component.scss'],
 })
--- apps/pokedex/src/app/modules/version/shared/components/version-group-picker/version-group-picker.component.ts
+++ apps/pokedex/src/app/modules/version/shared/components/version-group-picker/version-group-picker.component.ts
@@ -3,13 +3,14 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
 import { TranslocoModule } from '@jsverse/transloco';
 import { VersionGroupService } from '../../../../../api/games/version-group.service';
 import { InitialsPipe } from '../../../../../shared/pipes/initials.pipe';
 
 @Component({
   selector: 'pokedex-version-group-picker',
-  imports: [CommonModule, MatButtonModule, MatMenuModule, MatIconModule, InitialsPipe, TranslocoModule],
+  imports: [CommonModule, MatButtonModule, MatMenuModule, MatIconModule, InitialsPipe, TranslocoModule, MatTooltip],
   templateUrl: './version-group-picker.component.html',
   styleUrls: ['./version-group-picker.component.scss'],
 })
