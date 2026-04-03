462f7f9386fd9bde19739c7a41b7eb21c4c23a93
Francisco Sanchez <frango9000@gmail.com>
2026-04-03 16:36:52 +0200

    refactor: replace mat-icon with FontAwesome icon in sidenav component

--- apps/pokedex/src/app/core/components/sidenav.component.ts
+++ apps/pokedex/src/app/core/components/sidenav.component.ts
@@ -4,16 +4,28 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { TranslocoModule } from '@jsverse/transloco';
 import { IsMobileModule } from '../../shared/modules/is-mobile';
 import { SidenavService } from '../services/sidenav.service';
 
 @Component({
   selector: 'pokedex-sidenav',
-  imports: [CommonModule, MatSidenavModule, MatListModule, MatIconModule, TranslocoModule, IsMobileModule, RouterLink],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    TranslocoModule,
+    IsMobileModule,
+    RouterLink,
+    FaIconComponent,
+  ],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
   protected readonly sidenavService = inject(SidenavService);
+  protected readonly faGithub = faGithub;
 }
