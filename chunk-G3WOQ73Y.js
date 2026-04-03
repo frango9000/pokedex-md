982fe572ad035de801fd1996f4ba1226711e339b
Github Deploy Workflow <frango9000@gmail.com>
2026-04-03 19:20:29 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/frango9000/pokedex-md/commit/568d4975c1aaaffd72ba8b66118ed16d90f75f8e
    GitHub Actions build: https://github.com/frango9000/pokedex-md/actions/runs/23958942321

---
+++ chunk-G3WOQ73Y.js
@@ -0,0 +1,1 @@
+import{Ca as a,Ud as i,ba as r}from"./chunk-AVU34DEK.js";var c=class e extends i{get name(){return"machine"}static \u0275fac=(()=>{let t;return function(n){return(t||(t=a(e)))(n||e)}})();static \u0275prov=r({token:e,factory:e.\u0275fac,providedIn:"root"})};export{c as a};
---
+++ chunk-SSPHBTXU.js
@@ -0,0 +1,1 @@
+function r(e){return Number(e?.split("/").filter(Boolean).pop()??0)}export{r as a};
