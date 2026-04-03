a1efe6b79119348770622e48a575f23d974470fa
Francisco Sanchez <frango9000@gmail.com>
2026-04-03 18:48:45 +0200

    fix: correct translation key for stat changes in detail view

--- apps/pokedex/src/app/api/pokemon/stat.service.ts
+++ apps/pokedex/src/app/api/pokemon/stat.service.ts
@@ -15,7 +15,7 @@ export class StatService extends TranslatedService<Stat, PxStat> {
   protected override _parseAllTranslations(resources: PxStat[]): Observable<MergingMap> {
     return of(
       MergingMap.ofMultipleResources(resources, 'names', (stat, localized) => ({
-        STAT: { [stat.name]: localized },
+        stat: { [stat.name]: localized },
       })),
     );
   }
